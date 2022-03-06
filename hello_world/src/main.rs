f03e544b1321188b5c5afb155a778318f5947fcb
Ivan Costa <ivan.bravin@gmail.com>
2022-03-06 11:45:33 -0300

    Move gitignore to root folder

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,14 @@
-mod main_test;
+fn greet_world() {
+    println!("Hello, world!");
+    let southern_germany = "Grüß Gott!";
+    let japan = "ハロー・ワールド";
+    let portuguese = "Olá, Mundo!";
+    let regions = [southern_germany, japan, portuguese];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
